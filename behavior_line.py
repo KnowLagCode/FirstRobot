5c2f01fc2be082eaa7fbaa01c6d6268926a4c71c
Larry Galford <knowlagwizardry@gmail.com>
2023-04-30 18:26:55 -0400

    Adding the robot and beahvior line python files.

---
+++ behavior_line.py
@@ -0,0 +1,10 @@
+import robot
+from Raspi_MotorHAT import Raspi_MotorHAT
+from time import sleep
+
+zoomer = robot.Robot
+zoomer.leftMotor.setSpeed(150)
+zoomer.rightMotor.setSpeed(150)
+zoomer.leftMotor.run(Raspi_MotorHAT.FORWARD)
+zoomer.rightMotor.run(Raspi_MotorHAT.FORWARD)
+sleep(1)
---
+++ robot.py
@@ -0,0 +1,16 @@
+from Raspi_MotorHAT import Raspi_MotorHAT
+
+import atexit
+
+class Robot:
+    def __init__(self, motorhat_addr = 0x60):
+        self.motorHat = Raspi_MotorHAT(addr = motorhat_addr)
+
+        self.leftMotor = self.motorHat.getMotor(1)
+        self.rightMotor = self.motorHat.getMotor(2)
+
+        atexit.register(self.stop_motors)
+
+    def stop_motors(self):
+        self.leftMotor.run(Raspi_MotorHAT.RELEASE)
+        self.rightMotor.run(Raspi_MotorHAT.RELEASE)
