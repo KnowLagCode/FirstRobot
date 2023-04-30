5b98620a5b5b1b8645a127ed575c790930cf08fd
Larry Galford <knowlagwizardry@gmail.com>
2023-04-30 12:38:25 -0400

    added tst_motors py

---
+++ test_motors.py
@@ -0,0 +1,21 @@
+from Raspi_MotorHAT import Raspi_MotorHAT
+
+import time
+import atexit
+
+motorHat = Raspi_MotorHAT(address=0x60)
+leftMotor = motorHat.getMotor(1)
+rightMotor = motorHat.getMotor(2)
+
+def turn_off_motors():
+    leftMotor.run(Raspi_MotorHAT.RELEASE)
+    rightMotor.run(Raspi_MotorHAT.RELEASE)
+
+atexit.register(turn_off_motors)
+
+leftMotor.setSpeed(150)
+rightMotor.setSpeed(150)
+
+leftMotor.run(Raspi_MotorHAT.FORWARD)
+rightMotor.run(Raspi_MotorHAT.FORWARD)
+time.sleep(1)
